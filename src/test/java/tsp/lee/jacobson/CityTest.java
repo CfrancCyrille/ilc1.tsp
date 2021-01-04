f747bb9e062fff04ed98af7a0035fda814644405
Shinshikun <57477905+Shinshikun@users.noreply.github.com>
2021-01-04 17:26:08 +0100

    Add files via upload

--- src/test/java/tsp/lee/jacobson/CityTest.java
+++ src/test/java/tsp/lee/jacobson/CityTest.java
@@ -4,11 +4,25 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class CityTest {
 
+public class CityTest {
+	
+	
+	City ville1 = new City(0,0);
+	City ville2 = new City(1,0);
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDistanceToSelfTrue() {
+		double expected = 0.0d;
+		double dist = ville1.distanceTo(ville1);
+		assertEquals(expected, dist, 0);
+	
+	}
+	
+	public void testDistanceToDifferentTrue() {
+		double expected = 1.0d;
+		double dist = ville1.distanceTo(ville2);
+		assertEquals(expected, dist, 0);
 	}
 
 }
