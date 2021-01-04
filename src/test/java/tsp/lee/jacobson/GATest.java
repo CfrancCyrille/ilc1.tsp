b71627a975e90536aebd2447a8fa93a281821c97
AxelCoutisson <axel.coutisson@hotmail.fr>
2021-01-04 17:29:33 +0100

    add all tests

--- src/test/java/tsp/lee/jacobson/GATest.java
+++ src/test/java/tsp/lee/jacobson/GATest.java
@@ -1,14 +1,34 @@
 package tsp.lee.jacobson;
 
+import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class GATest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test(expected = ArrayIndexOutOfBoundsException.class)
+	public void testEvolvePopulation_emptyPopulation_shouldReturnArrayIndexOutOfBoundsException(){
+		Population pop = new Population(0, true);
+		Population actual = GA.evolvePopulation(pop);
 	}
+
+	/* sadly don't work
+	@Test
+	public void testEvolvePopulation_PopulationWith2Individuals_shouldReturn18(){
+		City city1 = new City(0, 0);
+		City city2 = new City(0, 3);
+		City city3 = new City(0, 9);
+		ArrayList<City> arrayCity = new ArrayList<City>();
+		arrayCity.add(city1);
+		arrayCity.add(city2);
+		arrayCity.add(city3);
+		Tour tour = new Tour(arrayCity);
+		Population pop = new Population(1, true);
+		pop.saveTour(0, tour);
+		double expected = 18.0;
+		double actual = GA.evolvePopulation(pop).getFittest().getFitness();
+		assertEquals(expected, actual, 0);
+	}*/
 
 }
