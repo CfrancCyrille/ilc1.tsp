7eda27d87a1844d3298dc19c727fbbabf87cbf41
zephyr-bat <56952800+zephyr-bat@users.noreply.github.com>
2021-01-04 16:12:59 +0100

    Ajout des test pour la classe City
    Ajout des test pour la fonction getDistanceTo()
    Franck GUILLERME

--- src/test/java/tsp/lee/jacobson/CityTest.java
+++ src/test/java/tsp/lee/jacobson/CityTest.java
@@ -5,10 +5,20 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CityTest {
-
+        City Ville1 = new City(2,2);
+        City Ville2 = new City(2,3);
+        
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void Test_getDistanceTo_VilleIdentique_true() {           
+            double expected = 0;
+            double result = Ville1.distanceTo(Ville1);
+            assertEquals(expected,result,0);
 	}
 
+        @Test
+	public void Test_getDistanceTo_VilleDifferente_true() {       
+            double expected = 1;
+            double result = Ville1.distanceTo(Ville2);
+            assertEquals(expected,result,0);
+	}
 }
