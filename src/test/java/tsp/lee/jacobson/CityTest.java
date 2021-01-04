a06987230f1146bde088cabf6c7ad34614f613fb
HugoBlain <hugo.blain@wanadoo.fr>
2021-01-04 16:59:14 +0100

    Test dans les classes Tour et City

--- src/test/java/tsp/lee/jacobson/CityTest.java
+++ src/test/java/tsp/lee/jacobson/CityTest.java
@@ -6,9 +6,48 @@ import org.junit.Test;
 
 public class CityTest {
 
+	// attributs utiles pour les tests
+	City ville1 = new City(0, 0);
+	City ville2 = new City(0, 2);
+	City ville3 = new City(2, 0);
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void distanceTo_ville1FromVille1_0() {
+		double expected = 0;
+		double actual = ville1.distanceTo(ville1);
+		assertEquals(expected, actual, 0.0);
+	}
+
+	// test de la premiere ville à la deuxième
+	@Test
+	public void distanceTo_ville2FromVille1_2(){
+		double expected = 2;
+		double actual = ville1.distanceTo(ville2);
+		assertEquals(expected, actual, 0.0);
+	}
+
+	// test de la première à la troisième
+	@Test
+	public void distanceTo_ville3FromVille1_2(){
+		double expected = 2;
+		double actual = ville1.distanceTo(ville3);
+		assertEquals(expected, actual, 0.0);
+	}
+
+	// test du calcule avec une ville qui a des coordonnées négatives
+	@Test
+	public void distanceTo_NegativeFromVille1_2(){
+		City negative = new City(-2,0);
+		double expected = 2;
+		double actual = ville1.distanceTo(negative);
+		assertEquals(expected, actual, 0.0);
+	}
+
+
+	// test si l'argument donné est nul
+	@Test(expected=NullPointerException.class)
+	public void distanceTo_Null_NullPointerException() {
+		double actual = ville1.distanceTo(null);
 	}
 
 }
--- src/test/java/tsp/lee/jacobson/TourTest.java
+++ src/test/java/tsp/lee/jacobson/TourTest.java
@@ -4,11 +4,46 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class TourTest {
 
+
+	// attributs utiles pour les tests
+	City ville1 = new City(3,1);
+	City ville2 = new City(1,1);
+	ArrayList listeVille = new ArrayList();
+
+
+	// test si le Tour ne compte pas de ville, le Tour est vide
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void getDistance_aucuneVille_0() {
+		Tour tour = new Tour();
+		int expected = 0;
+		int actual = tour.getDistance();
+		assertEquals(expected, actual);
 	}
+
+	// test si le Tour ne contient qu'une seule ville
+	@Test
+	public void getDistance_uneVille_0() {
+		listeVille.add(ville1);
+		Tour tour = new Tour(listeVille);
+		int expected = 0;
+		int actual = tour.getDistance();
+		assertEquals(expected, actual);
+	}
+
+	// test si le Tour contient 2 villes
+	@Test
+	public void getDistance_ville1ToVille2_16() {
+		listeVille.add(ville1);
+		listeVille.add(ville2);
+		Tour tour = new Tour(listeVille);
+		int expected = 4;
+		int actual = tour.getDistance();
+		assertEquals(expected, actual);
+	}
+
 
 }
