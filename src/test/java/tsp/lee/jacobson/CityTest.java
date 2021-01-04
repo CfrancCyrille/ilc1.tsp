0d70e184df968316f3f67f5b44e90a4f04531763
Maxime <Maxime@LAPTOP-0RCPFJ26>
2021-01-04 16:42:48 +0100

    Test of distanceTo function

--- src/test/java/tsp/lee/jacobson/CityTest.java
+++ src/test/java/tsp/lee/jacobson/CityTest.java
@@ -5,10 +5,25 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CityTest {
+	
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	public void testDistanceToDifferentZero() {
+		City ville1 = new City(0, 0);
+		City ville2 = new City(2, 0);
+		double expected = 2;
+		double actual = ville2.distanceTo(ville1);
+		assertEquals(expected, actual, 0);
+	}
+	
+	@Test
+	
+	public void testDistanceToZero() {
+		City ville1 = new City(0, 0);
+		double expected = 0;
+		double actual = ville1.distanceTo(ville1);
+		assertEquals(expected, actual, 0);
 	}
 
 }
