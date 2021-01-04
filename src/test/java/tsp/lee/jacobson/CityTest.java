8bdabaefa0e8a50a09e327e3707f93426a4efc2c
WyderSevrin <sevrin.wyder@gmail.com>
2021-01-04 17:10:45 +0100

    Implémentation des tests pour les classes : CityTest, GATest, TourTest

--- src/test/java/tsp/lee/jacobson/CityTest.java
+++ src/test/java/tsp/lee/jacobson/CityTest.java
@@ -5,10 +5,28 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CityTest {
-
+        City c1 = new City(0,0);
+        
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+        public void testDistanceTo_DistanceSurMemeVille_0() {      
+                double expected = 0.0d;
+                double actual = c1.distanceTo(c1);
+                assertEquals(expected, actual,0.1);
+        }
+        
+        @Test
+        public void testDistanceTo_DistanceEntre2Villes_2() {
+                City c1 = new City(2,2);    
+                City c2 = new City(8,2);
+                double expected = 6.0d;
+                double actual = c1.distanceTo(c2);
+                assertEquals(expected, actual,0.1);
+        }
+        
+        @Test
+        public void testDistanceTo_DistanceTo_MemeDistanceDansLesDeuxSens() {
+            City c2 = new City(6,2);
+            assertEquals(c2.distanceTo(c1), c1.distanceTo(c2), 0.1);
+        }
 
 }
--- src/test/java/tsp/lee/jacobson/GATest.java
+++ src/test/java/tsp/lee/jacobson/GATest.java
@@ -1,5 +1,6 @@
 package tsp.lee.jacobson;
 
+import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -7,8 +8,53 @@ import org.junit.Test;
 public class GATest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEvolutionPopulation_EvolutionDeLaPopulationEn1Tour_RienNeChange() {
+                ArrayList<City> cities = new ArrayList();
+                cities.add(new City(2, 2));
+                cities.add(new City(6, 2));
+                Tour tour = new Tour(cities);
+                
+                Population pop = new Population(1,false);
+                pop.saveTour(0, tour);
+                Population resultat = GA.evolvePopulation(pop);
+
+                assertEquals(resultat.getTour(0).getCity(0), cities.get(0));
+                assertEquals(resultat.getTour(0).getCity(1), cities.get(1));
 	}
 
+        @Test
+        public void testEvolutionPopulation_PopulationDeTaille1_PasDeChangement() {
+            ArrayList<City> cities = new ArrayList();
+            cities.add(new City(0, 0));
+            cities.add(new City(2, 2));
+            cities.add(new City(6, 2));
+            Tour tour = new Tour(cities);
+
+            Population pop = new Population(1, false);
+            pop.saveTour(0, tour);
+            Population popEvoluer = GA.evolvePopulation(pop);
+
+            assertEquals(popEvoluer.getFittest(), pop.getFittest());
+            assertEquals(1, pop.populationSize());
+        }
+        
+        @Test
+        public void testEvolutionPopulation_2Tour_AvecLeMielleurEnPremier() {
+            ArrayList<City> cities = new ArrayList();
+            cities.add(new City(0, 0));
+            cities.add(new City(2, 2));
+            cities.add(new City(6, 2));
+            Tour bestTour = new Tour(cities);
+
+            ArrayList<City> cities2 = new ArrayList<>(cities);
+            cities2.add(new City(80, 60));
+            Tour worstTour = new Tour(cities2);
+
+            Population pop = new Population(2, false);
+            pop.saveTour(0, worstTour);
+            pop.saveTour(1, bestTour);
+            Population popEvoluer = GA.evolvePopulation(pop);
+
+            assertEquals(popEvoluer.getTour(0), bestTour);
+        }
 }
