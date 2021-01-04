e992580b72529f3af8edbed19199d2a6fe8c1b35
WaldnerCeline <50487569+WaldnerCeline@users.noreply.github.com>
2021-01-04 17:11:46 +0100

    tests complets

--- CityTest.java
+++ CityTest.java
@@ -7,8 +7,20 @@ import org.junit.Test;
 public class CityTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testdistanceToSameTrue() {
+            City city = new City(60, 200);
+            double distance = city.distanceTo(city);
+            double expected = 0;
+            assertEquals(expected, distance, 0.001);
+	}
+        
+        @Test
+	public void testdistanceToDifferentTrue() {
+            City city = new City(60, 200);
+            City city2 = new City(20, 40);
+            double distance = city.distanceTo(city2);
+            double expected = 164.92422502470643;
+            assertEquals(expected, distance, 0.001);
 	}
 
 }
